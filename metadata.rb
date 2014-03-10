1af5711083b06b1d66712099546a22a42295d912
Ryan Richard <ryanrichard07@gmail.com>
2014-03-10 11:34:37 -0500

    remove more aws, update berks and metdata

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
-name             'elasticsearch'
-
-maintainer       'karmi'
-maintainer_email 'karmi@karmi.cz'
-license          'Apache'
+name             'rackspac_elasticsearch'
+maintainer       'Rackspace'
+maintainer_email 'rackspace-cookbooks@rackspace.com'
+license          'Apache 2.0'
 description      'Installs and configures elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.7'
+version          '1.0.0'
 
-depends 'ark'
+depends 'ark', '~> 0.2'
+depends 'java', '~> 1.18'
 
 recommends 'build-essential'
 recommends 'xml'
